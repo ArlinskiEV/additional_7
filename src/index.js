b6914294f7d179d3050d8d4f72cc5d245670a155
ArlinskiEV <ArlinskiEV@gmail.com>
2018-03-01 04:48:01 +0300

    additional_7/features: solve

--- src/index.js
+++ src/index.js
@@ -1,52 +1,42 @@
 module.exports = function solveSudoku(matrix) {
   // your solution
-  class Sudoku {
-    constructor(desc) {
-      this.desc = {
-        items: desc
-          .reduce((prev, item) => [...prev, ...item], [])
-          .map(item => ({
-            current: item,
-            mayBe: item
-              ? []
-              : [1, 2, 3, 4, 5, 6, 7, 8, 9],
-          })),
-        rows: [[], [], [], [], [], [], [], [], []],
-        columns: [[], [], [], [], [], [], [], [], []],
-        squares: [[], [], [], [], [], [], [], [], []],
+  let items = matrix
+    .reduce((prev, item) => [...prev, ...item], [])
+    .map((item, index) => ({
+      current: item,
+      mayBe: item
+        ? []
+        : [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      row: Math.trunc(index / 9),
+      column: index % 9,
+      square: index,})
+    );
+
+  let flag = false;
+  do {
+    flag = false;
+    items
+    .filter(item => item.mayBe.length !== 0)
+    .forEach(item => {
+      // reduce mayBe array
+      item.mayBe = item.mayBe.filter(num => {
+        let inThis = items
+          .filter(i => ((i.row === item.row) || (i.column === item.column) || (i.square === item.square)))
+          .some(j => j.current === num);
+        return !(inThis);
+      });
+
+      if (item.mayBe.length === 1) {
+        item.current = item.mayBe.pop();
+        flag = true;
+        window.console.log(`row=${item.row} col=${item.column} current=${item.current}`);
       };
-      this.desc.items
-        .forEach((item, index) => {
-          // window.console.log(`this=${this}`);
-          // window.console.log(`index=${index} trunc/9: ${Math.trunc(index / 9)} %9:${index % 9} trunc/3: ${Math.trunc(index / 3)} %3 ${index % 3}`);
-          this.desc.rows[Math.trunc(index / 9)][index % 9] = item;
-          this.desc.columns[index % 9][Math.trunc(index / 9)] = item;
-          // this.desc.squares[][] = item;
-        });
-      
-      this.desc.items
-        .forEach((item, index) => {
-          if (item.current === 0) {
-            // ---------------------------------------------------------
-            this.desc.rows.forEach((a, i) => {
-              if (a.indexOf(item) !== -1) {
-                window.console.log(`r: ${i}`);
-              }
-              return 0;
-            });
-            this.desc.columns.forEach((a, i) => {
-              if (a.indexOf(item) !== -1) {
-                window.console.log(`c: ${i}`);
-              }
-              return 0;
-            });
-            window.console.log(`mayBe:${item.mayBe}`);
-            // ---------------------------------------------------------
-            let row = this.desc.rows.findIndex();
-            let column = [];
-            // let square = [];
-          }
-        });
-    }
-  }
+    });
+  } while (flag);
+  
+  return items
+    .reduce((prev, item, index) => {
+      prev[Math.trunc(index / 9)][index % 9] = item.current;
+      return prev;
+    }, [[], [], [], [], [], [], [], [], []]);
 }
