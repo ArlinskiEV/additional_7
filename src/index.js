66e3285e557da61ffe012fcfca685ac2189a26d3
ArlinskiEV <ArlinskiEV@gmail.com>
2018-03-01 17:13:53 +0300

    additional_7/fix: square

--- src/index.js
+++ src/index.js
@@ -9,30 +9,38 @@ module.exports = function solveSudoku(matrix) {
         : [1, 2, 3, 4, 5, 6, 7, 8, 9],
       row: Math.trunc(index / 9),
       column: index % 9,
-      square: index,})
+      square: (Math.trunc(Math.trunc(index / 9) / 3) * 3) + Math.trunc((index % 9) / 3),})
     );
 
-  let flag = false;
-  do {
-    flag = false;
-    items
-    .filter(item => item.mayBe.length !== 0)
-    .forEach(item => {
-      // reduce mayBe array
-      item.mayBe = item.mayBe.filter(num => {
-        let inThis = items
-          .filter(i => ((i.row === item.row) || (i.column === item.column) || (i.square === item.square)))
-          .some(j => j.current === num);
-        return !(inThis);
-      });
+  // items.forEach(item => {
+  //   window.console.log(`row=${item.row} col=${item.column} square=${item.square} current=${item.current}`);
+  // });
 
-      if (item.mayBe.length === 1) {
-        item.current = item.mayBe.pop();
-        flag = true;
-        // window.console.log(`row=${item.row} col=${item.column} current=${item.current}`);
-      };
-    });
-  } while (flag);
+  function simple() {
+    let flag = false;
+    do {
+      flag = false;
+      items
+      .filter(item => item.mayBe.length !== 0)
+      .forEach(item => {
+        // reduce mayBe array
+        item.mayBe = item.mayBe.filter(num => {
+          let inThis = items
+            .filter(i => ((i.row === item.row) || (i.column === item.column) || (i.square === item.square)))
+            .some(j => j.current === num);
+          return !(inThis);
+        });
+
+        if (item.mayBe.length === 1) {
+          item.current = item.mayBe.pop();
+          flag = true;
+          // window.console.log(`row=${item.row} col=${item.column} square=${item.square} current=${item.current}`);
+        };
+      });
+    } while (flag);
+  }
+
+  simple();
   
   return items
     .reduce((prev, item, index) => {
